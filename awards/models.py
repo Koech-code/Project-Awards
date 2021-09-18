70e0e1c090d828a9dd367a05a4989e15e3bbe54e
Koech-code <nixon.koech@moringaschool.com>
2021-09-18 23:23:29 +0300

    create models to handle data

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.fields import CharField
+from pyuploadcare.dj.models import ImageField
+ 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture=models.ImageField(upload_to='images', default='no picture', null=True, blank=True)
+    name=models.CharField(max_length=30)
+    bio=models.TextField(max_length=500)
+
+class Projects(models.Model):
+    title=models.CharField(max_length=60)
+    image=ImageField(manual_crop='1280x720')
+    description=models.TextField(max_length=500)
+    link=models.URLField(max_length=255)
+    date=models.DateField(auto_now_add=True, blank=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
