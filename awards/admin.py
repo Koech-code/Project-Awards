ff08da7f3f5e34c786b1682ce1826b5b29a4a0e9
Koech-code <nixon.koech@moringaschool.com>
2021-09-19 16:04:47 +0300

    register the ratings model

--- awards/admin.py
+++ awards/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Projects, Profile
+from .models import Projects, Profile, Ratings
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Projects)
+admin.site.register(Ratings)
