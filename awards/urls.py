27c5cb0d3fc11b4ce064a12383bf7dbf940065c7
Koech-code <nixon.koech@moringaschool.com>
2021-09-20 12:54:20 +0300

    create a url for the uploads

--- awards/urls.py
+++ awards/urls.py
@@ -8,9 +8,14 @@ from . import views
 urlpatterns=[
     url('^$', views.index, name='index'),
     url('profile/' ,views.profile, name='profile'),
+    url('search/' ,views.search_project, name='search'),
+    url('logout/', LogoutView.as_view(), {"next_page":''}),
     url(r'^api/projects/$', views.ProjList.as_view()),
     url(r'^api/profiles/$', views.ProfList.as_view()),
+    url(r'^rates/<project>', views.rates, name='rates'),
+    url('uploads/' ,views.upload, name='uploads'),
 ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
