edd7335168bf6e7c2e7c75b5e3ac68e4b11c901c
Koech-code <nixon.koech@moringaschool.com>
2021-09-20 12:52:54 +0300

    create a methods for used in testing

--- awards/models.py
+++ awards/models.py
@@ -10,6 +10,14 @@ class Profile(models.Model):
     name=models.CharField(max_length=30)
     bio=models.TextField(max_length=500)
 
+    def delete_profile(self):
+        self.delete()
+
+    
+    def save_profile(self):
+        self.save()
+
+
 class Projects(models.Model):
     title=models.CharField(max_length=60)
     image=ImageField(manual_crop='1280x720')
@@ -17,6 +25,24 @@ class Projects(models.Model):
     link=models.URLField(max_length=255)
     date=models.DateField(auto_now_add=True, blank=True)
     user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_projects(cls):
+        return cls.objects.all()
+
+    def save_project(self):
+        self.save()
+
 
 class Ratings(models.Model):
     rating=((1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(10,'10'),)
@@ -29,6 +55,18 @@ class Ratings(models.Model):
     score=models.FloatField(default=0, blank=True)
     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     project=models.ForeignKey(Projects, on_delete=models.CASCADE, null=True)
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Ratings.objects.filter(project_id=id).all()
+        return ratings(len(ratings) == 1)
+
+    def __str__(self):
+        return f'{self.post} Rating'
+
 
 
 
